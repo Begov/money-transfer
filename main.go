576f4680be5bf37c325e9c7b8cd814feffec3608
Emran <begovon@gmail.com>
2025-12-11 17:53:06 +0300

    -

--- main.go
+++ main.go
@@ -24,20 +24,73 @@ func (u *User) Withdraw(coins float64) error {
 	return nil
 }
 
+type Transaction struct {
+	FromID string
+	ToID   string
+	Amount float64
+}
+
+type PaymentSystem struct {
+	Users        map[string]User
+	Transactions []Transaction
+}
+
+func (ps *PaymentSystem) AddUser(u User) {
+	if ps.Users == nil {
+		ps.Users = make(map[string]User)
+	}
+	ps.Users[u.ID] = u
+}
+
+func (ps *PaymentSystem) AddTransaction(t Transaction) {
+	ps.Transactions = append(ps.Transactions, t)
+}
+
+func (ps *PaymentSystem) ProcessingTransactions() error {
+	for _, t := range ps.Transactions {
+		fromUser, fromExist := ps.Users[t.FromID]
+		toUser, toExist := ps.Users[t.ToID]
+
+		if !fromExist {
+			return fmt.Errorf("user with ID %s not found", t.FromID)
+		}
+		if !toExist {
+			return fmt.Errorf("user with ID %s not found", t.ToID)
+		}
+
+		if err := fromUser.Withdraw(t.Amount); err != nil {
+			return fmt.Errorf("error withdrawing from user %s: %v", t.FromID, err)
+		}
+
+		toUser.Deposit(t.Amount)
+
+		ps.Users[t.FromID] = fromUser
+		ps.Users[t.ToID] = toUser
+	}
+
+	ps.Transactions = nil
+	return nil
+}
+
 func main() {
+
+	ps := &PaymentSystem{
+		Users:        make(map[string]User),
+		Transactions: []Transaction{},
+	}
+
 	user1 := &User{ID: "1", Name: "Иван", Balance: 400}
 	user2 := &User{ID: "2", Name: "Артем", Balance: 1268.04}
 
-	user1.Deposit(200)
+	ps.AddUser(*user1)
+	ps.AddUser(*user2)
 
-	if err := user1.Withdraw(700); err != nil {
-		fmt.Printf("%s: %s\n", user1.Name, err)
+	ps.AddTransaction(Transaction{"1", "2", 200})
+	ps.AddTransaction(Transaction{"2", "1", 50})
+
+	if err := ps.ProcessingTransactions(); err != nil {
+		fmt.Println("Error processing transactions:", err)
+		return
 	}
 
-	if err := user2.Withdraw(700); err != nil {
-		fmt.Printf("%s: %s\n", user2.Name, err)
-	}
-
-	fmt.Printf("%s: %.2f на балансе.\n", user1.Name, user1.Balance)
-	fmt.Printf("%s: %.2f на балансе.\n", user2.Name, user2.Balance)
 }
